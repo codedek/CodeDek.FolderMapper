c7c6dd049dcda29a7465c17e88b5cde6a483d943
CodeDek <codedek@gmail.com>
2019-06-16 21:05:54 -0400

    Update: updated assembly info

--- src/FolderMapper/Properties/AssemblyInfo.cs
+++ src/FolderMapper/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FolderMapper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Easily maps a windows folder to a drive letter.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CodeDek")]
 [assembly: AssemblyProduct("FolderMapper")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("© 2019 CodeDek. All Rights Reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -53,3 +53,5 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.2.0.0")]
 [assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
