771d0c2f59df86fb694cfcc0b9615353ecfd03d5
CodeDek <codedek@gmail.com>
2019-06-26 10:02:59 -0400

    Update: added support for filing issues

--- src/FolderMapper/ViewModels/AboutViewModel.cs
+++ src/FolderMapper/ViewModels/AboutViewModel.cs
@@ -11,6 +11,7 @@ namespace FolderMapper.ViewModels
         public byte[] AppIcon { get; set; } = File.ReadAllBytes(@"D:\.repo\_product\CodeDek.FolderMapper\art\ic_folder_mapper.ico");
         public string Home { get; set; } = "https://github.com/codedek/CodeDek.FolderMapper";
         public string Download { get; set; } = "https://github.com/codedek/CodeDek.FolderMapper/releases";
+        public string Issues { get; set; } = "https://github.com/codedek/CodeDek.FolderMapper/issues";
         public string UpdateUrl { get; set; }
         public string AppName => "Folder Mapper";
         public string AppVersion => $"v{FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion}";
@@ -19,5 +20,6 @@ namespace FolderMapper.ViewModels
 
         public Cmd NavigateHomeUrlCmd => new Cmd(() => Process.Start(Home));
         public Cmd NavigateDownloadUrlCmd => new Cmd(() => Process.Start(Download));
+        public Cmd NavigateIssuesUrlCmd => new Cmd(() => Process.Start(Issues));
     }
 }
