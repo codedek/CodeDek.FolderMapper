1c5e0dbd1b3d84bc3398a07bc684f6373a03e23f
CodeDek <codedek@gmail.com>
2019-06-28 07:21:41 -0400

    added license and changelog links

--- src/FolderMapper/ViewModels/AboutViewModel.cs
+++ src/FolderMapper/ViewModels/AboutViewModel.cs
@@ -12,7 +12,8 @@ namespace FolderMapper.ViewModels
         public string Home { get; set; } = "https://github.com/codedek/CodeDek.FolderMapper";
         public string Download { get; set; } = "https://github.com/codedek/CodeDek.FolderMapper/releases";
         public string Issues { get; set; } = "https://github.com/codedek/CodeDek.FolderMapper/issues";
-        public string UpdateUrl { get; set; }
+        public string License => "https://github.com/codedek/CodeDek.FolderSharer/blob/master/LICENSE";
+        public string Changelog => "https://github.com/codedek/CodeDek.FolderSharer/blob/master/CHANGELOG.md";
         public string AppName => "Folder Mapper";
         public string AppVersion => $"v{FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion}";
         public string Copyright => "© 2019 CodeDek. All Rights Reserved";
@@ -21,5 +22,7 @@ namespace FolderMapper.ViewModels
         public Cmd NavigateHomeUrlCmd => new Cmd(() => Process.Start(Home));
         public Cmd NavigateDownloadUrlCmd => new Cmd(() => Process.Start(Download));
         public Cmd NavigateIssuesUrlCmd => new Cmd(() => Process.Start(Issues));
+        public Cmd NavigateLicenseUrlCmd => new Cmd(() => Process.Start(License));
+        public Cmd NavigateChangelogUrlCmd => new Cmd(() => Process.Start(Changelog));
     }
 }
